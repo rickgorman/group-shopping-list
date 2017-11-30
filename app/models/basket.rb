030fd7390a8c5d7380167434d0074ae442c454e0
Rick Gorman <gormanite@gmail.com>
2017-11-30 02:29:55 -0500

    Rewrote query using raw SQL to order based on basket_items.created_at

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -6,10 +6,25 @@ class Basket < ApplicationRecord
   has_many :items,
     through: :basket_items
 
-  # Returns an array of items grouped so as to show quantity. An array is used
-  # in order to preserve ordering. Example:
-  #   [ ['tomato', 3], ['head of lettuce', 1], ... ]
+  # Returns an array of objects grouped so as to show quantity. An array is used
+  # to preserve ordering.
   def items_with_quantities
-    self.items.group(:id).order(:created_at).count.to_a
+    ActiveRecord::Base.connection.execute(<<-SQL)
+      SELECT
+        items.id AS items_id,
+        items.name AS items_name,
+        COUNT(*) AS count_all
+      FROM
+        "basket_items"
+      LEFT JOIN
+        items ON items.id = basket_items.item_id
+      WHERE
+        "basket_items"."basket_id" = 1
+      GROUP BY
+        items.id,
+        items.name
+      ORDER BY
+        MIN(basket_items.created_at);
+SQL
   end
 end
