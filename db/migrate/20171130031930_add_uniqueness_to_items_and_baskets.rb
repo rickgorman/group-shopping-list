d4a7c71c820812e70df39278c6fb6bc1c11f3367
Rick Gorman <gormanite@gmail.com>
2017-11-29 22:23:28 -0500

    Add uniqueness to names of items and baskets

--- db/migrate/20171130031930_add_uniqueness_to_items_and_baskets.rb
+++ db/migrate/20171130031930_add_uniqueness_to_items_and_baskets.rb
@@ -1,9 +1,6 @@
 class AddUniquenessToItemsAndBaskets < ActiveRecord::Migration[5.1]
   def change
-    remove_index :items, :name
     add_index :items, :name, unique: true
-
-    remove_index :baskets, :name
     add_index :baskets, :name, unique: true
   end
 end
