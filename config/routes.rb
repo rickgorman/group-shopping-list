5ef4d11d3cf55b20adbfc85df260b8f8aa97e7ed
Rick Gorman <gormanite@gmail.com>
2017-11-29 20:36:37 -0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do
+    resources :baskets, only: [:create, :index, :show, :update, :destroy] do
+      resources :items, only: [:create, :index]
+    end
+
+    resources :items, only: [:show, :update, :destroy]
+  end
+
 end
