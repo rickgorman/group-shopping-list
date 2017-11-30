e9200a47e17bb398358b453da47dd8ed9e5b3d84
Rick Gorman <gormanite@gmail.com>
2017-11-29 23:20:30 -0500

    Add route for entry html page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root 'static_pages#root'
+
   namespace :api, defaults: { format: :json } do
     resources :baskets, only: [:create, :index, :show, :update, :destroy] do
       resources :items, only: [:create, :index]
