e52fa77f90d8f441ef62af42664db2128cbe3433
Rick Gorman <gormanite@gmail.com>
2017-11-29 21:24:47 -0500

    Retain order of items as they are added

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -9,6 +9,6 @@ class Basket < ApplicationRecord
   # returns a hash of items grouped so as to show quantity.
   #   example: { 'tomato' => 3, 'head of lettuce' => 1 }
   def items_with_quantities
-    self.items.group(:name).count
+    self.items.group(:name).order(:created_at).count
   end
 end
