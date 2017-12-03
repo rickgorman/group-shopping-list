4111ba5bd08f5cee405c6eeeb96962f88cb07464
Rick Gorman <gormanite@gmail.com>
2017-12-02 20:53:40 -0500

    Add Item CRUD actions

--- frontend/actions/item_actions.js
+++ frontend/actions/item_actions.js
@@ -1,6 +1,9 @@
+import * as ApiUtil from '../util/item_api_util';
+
 export const RECEIVE_ITEM = "RECEIVE_ITEM";
 export const RECEIVE_ITEMS = "RECEIVE_ITEMS";
-export const DESTROY_ITEM = "DESTROY_ITEM";
+export const RECEIVE_ITEM_ERRORS = "RECEIVE_ITEM_ERRORS";
+export const REMOVE_ITEM = "REMOVE_ITEM";
 
 const receiveItem = (item) => {
   return {
@@ -16,26 +19,56 @@ const receiveItems = (items) => {
   };
 };
 
-export const createItem = (item) => {
-  // ajax
+const receiveItemErrors = (errors) => {
   return {
-    type: RECEIVE_ITEM,
+    type: RECEIVE_ITEM_ERRORS,
+    errors
+  };
+};
+
+const removeItem = (item) => {
+  return {
+    type: REMOVE_ITEM,
     item
   };
 };
 
-export const updateItem = (item) => {
-  // ajax
-  return {
-    type: RECEIVE_ITEM,
-    item
-  };
+export const createItem = (item) => dispatch => {
+  ApiUtil.createItem(item)
+  .then((resItem) => {
+    return dispatch(receiveItem(resItem));
+  })
+  .fail((errors) => {
+    return dispatch(receiveItemErrors(errors.responseJSON));
+  });
 };
 
-export const destroyItem = (item) => {
-  // ajax
-  return {
-    type: DESTROY_ITEM,
-    item
-  };
+export const fetchItems = (basketId = 1) => dispatch => {
+  ApiUtil.fetchItems(basketId)
+  .then((resItems) => {
+    return dispatch(receiveItems(resItems));
+  })
+  .fail((errors) => {
+    return dispatch(receiveItemErrors(errors.responseJSON));
+  });
+};
+
+export const updateItem = (item) => dispatch => {
+  ApiUtil.updateItem(item)
+  .then((resItem) => {
+    return dispatch(receiveItem(resItem));
+  })
+  .fail((errors) => {
+    return dispatch(receiveItemErrors(errors));
+  });
+};
+
+export const destroyItem = (item) => dispatch => {
+  ApiUtil.destroyItem(item)
+  .then((resItem) => {
+    return dispatch(removeItem(item));
+  })
+  .fail((errors) => {
+    return dispatch(receiveItemErrors(errors));
+  });
 };
