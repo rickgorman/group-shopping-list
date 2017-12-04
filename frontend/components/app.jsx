f2ca2c333cd5a6ed31824818f6e495ba38274962
Rick Gorman <gormanite@gmail.com>
2017-12-04 01:47:32 -0500

    Add Footer and clean up css

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import BasketContainer from './basket_container.jsx'
+import Footer from './footer.jsx';
 
 class App extends React.Component {
   render() {
     return(
       <div className='app-wrapper'>
         <BasketContainer basketId={1} />
+        <Footer />
       </div>
     );
   }
---
+++ frontend/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return(
+    <div className='footer'>
+      <p>©&nbsp;
+        <a href="//linkedin.com/in/rickgormannyc/">Rick Gorman</a> 2017
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
